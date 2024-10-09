138c48f944fd9ddac5b42f343ababb7e287e1bab
Anselm Powell <anselm.powell@sport.wales>
2024-10-09 12:28:03 +0100

    test 4

--- src/api/views.py
+++ src/api/views.py
@@ -26,12 +26,12 @@ def check_allowed_domains(request):
         return parsed.netloc in allowed_domains or parsed.netloc.split(':')[0] in allowed_domains
 
     # Check if the request is coming from an allowed host
-    # if host in allowed_domains:
-    #     return True
+    if host in allowed_domains:
+        return True
 
     # Check if the request has a valid Origin header
-    if origin and domain_match(origin):
-        return True
+    # if origin and domain_match(origin):
+    #     return True
 
     # If no Origin, check Referer as a fallback
     if not origin and referer and domain_match(referer):
