97a04aa1e549ffdf49d4704b1f2eb5bd30ebdac8
Anselm Powell <anselm.powell@sport.wales>
2024-10-09 11:40:49 +0100

    Improved access permissions to allow self migrations

--- src/api/permissions.py
+++ src/api/permissions.py
@@ -2,30 +2,26 @@ from rest_framework.permissions import BasePermission
 from rest_framework.exceptions import PermissionDenied
 
 class AllowSpecificDomainPermission(BasePermission):
+    allowed_domains = [
+        'baseinterface-production.up.railway.app',
+        'basedatastore-production.up.railway.app',
+    ]
+
     def has_permission(self, request, view):
-        allowed_domains = [
-            'baseinterface-production.up.railway.app',
-            'https://baseinterface-production.up.railway.app',
-            'http://baseinterface-production.up.railway.app',
-            'basedatastore-production.up.railway.app',
-            'https://basedatastore-production.up.railway.app',
-            'http://basedatastore-production.up.railway.app',
-            # Add more allowed domains here
-        ]
+        host = request.get_host().lower()
+        origin = request.headers.get('Origin', '').lower()
+        referer = request.headers.get('Referer', '').lower()
         
         # Allow access if the request is coming from the same host
-        if request.get_host() in allowed_domains:
+        if any(host == domain.lower() for domain in self.allowed_domains):
             return True
         
-        origin = request.headers.get('Origin')
-        referer = request.headers.get('Referer')
-        
         # Check if the request is coming from an allowed origin
-        if origin and any(domain in origin for domain in allowed_domains):
+        if origin and any(origin.endswith(domain.lower()) for domain in self.allowed_domains):
             return True
         
         # If no Origin, check Referer as a fallback
-        if not origin and referer and any(domain in referer for domain in allowed_domains):
+        if not origin and referer and any(referer.endswith(domain.lower()) for domain in self.allowed_domains):
             return True
         
         # If it's a same-origin request (e.g., from the Django admin or API root)
